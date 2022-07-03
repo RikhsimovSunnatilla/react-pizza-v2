3ec22c1abedd7e8bf445173fb171dff41d626819
RikhsimovSunnatilla <rixsimovsunnatilla@gmail.com>
2022-07-03 12:47:08 +0500

    first commit

---
+++ src/components/Categories.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Categories({ value, onChangeCategory }) {
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((categoryName, i) => (
+          <li key={i} className={value === i ? 'active' : ''} onClick={() => onChangeCategory(i)}>
+            {categoryName}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
